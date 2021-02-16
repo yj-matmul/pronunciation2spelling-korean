a3d632e95a3b4faccbcc60e7f375657274802c67
yj-matmul <yyj3053@gmail.com>
2021-02-16 23:49:54 +0900

    [20210216] use only tokenizer decode method

--- run_electra_predict.py
+++ run_electra_predict.py
@@ -26,14 +26,11 @@ def predict(config, tokenizer, model, text):
         output = output.squeeze(0).max(dim=-1, keepdim=False)[1]
         next_idx = output[i].item()
     ids = dec_ids.to('cpu').numpy()
-    tokens = tokenizer.convert_ids_to_tokens(ids)
-    result = tokenizer.convert_tokens_to_string(tokens)
-    result2 = tokenizer.decode(ids)
+    output = tokenizer.decode(ids)
     print('소요 시간:', time.time() - start)
     print('원래 문장:', text)
-    print('변환 문장:', result)
-    print(result2)
-    return result
+    print('변환 문장:', output)
+    return output
 
 
 class Pronunciation2Spelling(nn.Module):
